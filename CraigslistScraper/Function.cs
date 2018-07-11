2ca73a3ae39d25b95450df8fa04047a02bc87bfd
pedroadiaz <pedroadiaz@hotmail.com>
2018-07-10 22:31:29 -0700

    Qa (#1)
    * Updating the code to fix the parsing and saving to DynamoDB
    
    * Refactoring and adding different UR:s
    
    * Fixed some things in order to publish
    
    * Changed DBContext to private and for some reason, that fixed a bug
    
    * Fixing a bug in the response body.
    
    * Changing the primary key to the Craigslist data-id

--- CraigslistScraper/Function.cs
+++ CraigslistScraper/Function.cs
@@ -1,24 +1,42 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace CraigslistScraper
 {
-    public class Functions
+    public class Function
     {
+        public const string CITY_NAME = "city";
+        IDynamoDBContext DBContext { get; set; }
+
+        private string GigURL = "https://{0}.craigslist.org/search/cpg?is_paid=yes&postedToday=1";
+
+        private string JobURL = "https://{0}.craigslist.org/search/sof?postedToday=1&is_telecommuting=1";
+
         /// <summary>
         /// Default constructor that Lambda will invoke.
         /// </summary>
-        public Functions()
+        public Function()
         {
+            string tableName = "CraigslistJobs";
+            AWSConfigsDynamoDB.Context.TypeMappings[typeof(Listing)] = new Amazon.Util.TypeMapping(typeof(Listing), tableName);
+            DynamoDBContextConfig config = new DynamoDBContextConfig { Conversion = DynamoDBEntryConversion.V2 };
+            this.DBContext = new DynamoDBContext(new AmazonDynamoDBClient(), config);
         }
 
 
@@ -27,7 +45,7 @@ namespace CraigslistScraper
         /// </summary>
         /// <param name="request"></param>
         /// <returns>The list of blogs</returns>
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> Get(APIGatewayProxyRequest request, ILambdaContext context)
         {
             context.Logger.LogLine("Get Request\n");
 
@@ -39,6 +57,137 @@ namespace CraigslistScraper
             };
 
             return response;
+        }
+
+        public async Task<APIGatewayProxyResponse> AddListing(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            string cityName = null;
+            APIGatewayProxyResponse response = new APIGatewayProxyResponse();
+            try
+            {
+                if (request.QueryStringParameters != null && request.QueryStringParameters.ContainsKey(CITY_NAME))
+                {
+                    cityName = request.QueryStringParameters[CITY_NAME];
+                }
+                else
+                {
+                    cityName = "losangeles";
+                    if (request.QueryStringParameters != null)
+                    {
+                        Console.WriteLine("Querystring parameters");
+                        foreach (string something in request.QueryStringParameters.Keys)
+                        {
+                            Console.WriteLine(something);
+                        }
+                    }
+                    if (request.PathParameters != null)
+                    {
+                        Console.WriteLine("Path parameters");
+                        foreach (string something in request.PathParameters.Keys)
+                        {
+                            Console.WriteLine(something);
+                        }
+                    }
+                }
+
+                string content = GetData(cityName, this.GigURL);
+                List<Listing> listings = ParseHTML(content, cityName);
+
+                string jobContent = GetData(cityName, this.JobURL);
+                listings.AddRange(ParseHTML(jobContent, cityName));
+
+                foreach (Listing listing in listings)
+                {     
+                    await DBContext.SaveAsync<Listing>(listing);
+                }
+
+                response.StatusCode = (int)HttpStatusCode.OK;
+                response.Body = string.Format("{0} listings saved in {1}", listings.Count, cityName);
+                response.Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } };
+            }
+            catch (Exception e)
+            {
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Body = string.Format("0 listings saved. Error: {0}\r\n{1}", e.Message, e.StackTrace);
+                response.Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } };
+            }
+
+            return response;
+        }
+
+        private List<Listing> ParseHTML(string content, string cityName)
+        {
+            string regexAnchor = @"<a href=""([\w\:\.\-\/]+\d+\.html)"" data\-id=""(\d+)""[^>]*?>(.+)(?=</a>)";
+            string regexDate = @"<time class=""result-date"" datetime=""(\d{4}-\d{2}-\d{2} \d{2}:\d{2})""[^>]*?>";
+            List<Listing> listings = new List<Listing>();
+
+            if (content.Contains("<li class=\"result-row\""))
+            {
+                MatchCollection dateMatches = Regex.Matches(content, regexDate);
+                MatchCollection anchorMatches = Regex.Matches(content, regexAnchor);
+
+                if (dateMatches != null && dateMatches.Count > 0)
+                {
+                    for (int i = 0; i < dateMatches.Count; i++)
+                    {
+                        Match match = dateMatches[i];
+                        for (int groupIndex = 0; groupIndex < match.Groups.Count; groupIndex++)
+                        {
+                            Group group = match.Groups[groupIndex];
+                            if (!group.Value.StartsWith("<time"))
+                            {
+                                Listing listing = new Listing();
+                                listing.ID = anchorMatches[i].Groups[groupIndex + 1].Value;
+                                listing.ListingDate = group.Value;
+                                listing.Title = anchorMatches[i].Groups[groupIndex + 2].Value;
+                                listing.Link = anchorMatches[i].Groups[groupIndex].Value;
+                                listing.City = cityName;
+                                listings.Add(listing);
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            return listings;
+        }
+        private string GetData(string city, string URL)
+        {
+            StreamReader reader = null;
+            string content = null;
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(string.Format(URL, city));
+                httpWebRequest.ContentType = "";
+                httpWebRequest.Method = "GET";
+                WebResponse webResponse = httpWebRequest.GetResponse();
+
+                reader = new StreamReader(webResponse.GetResponseStream());
+                content = reader.ReadToEnd();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception:");
+                    Console.WriteLine(ex.InnerException.Message);
+                    Console.WriteLine(ex.InnerException.StackTrace);
+                }
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                reader = null;
+            }
+           
+            return content;
         }
     }
 }
---
+++ CraigslistScraper/Listing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CraigslistScraper
+{
+    public class Listing
+    {
+        public Listing()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+        public string ID { get; set; }
+        public string ListingDate { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string City { get; set; }
+    }
+}
